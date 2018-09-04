d7b4c30c9c46f5857f230ed8ef20b0b572af91b7
Anirudh Rajagopalan <anirudhrajagopalan@Anirudhs-MacBook-Pro.local>
2018-09-04 01:50:20 -0400

    commit2

--- purchaseData.py
+++ purchaseData.py
@@ -1,13 +1,20 @@
-import os 
+import os
 import pandas as pd
 import numpy as np
 
-purchase_data = "Resources/purchase_data.csv"
+purchase_data = ('/Users/anirudhrajagopalan/Desktop/DataVizClassRepo/RUTSOM201807DATA5/Tuesday-Thursday/04 - Python Pandas/HW4/HeroesOfPymoli/Resources/purchase_data.csv')
 
-purchase_data = pd.read_csv(purchase_data.csv)
+purchaseData = pd.read_csv(purchase_data)
 
-purData = pd.Dataframe(purchase_data)
+purData = pd.DataFrame(purchaseData)
 
-def purchaseStats():
+def playerCount():
     totalPlayers = purData.groupby(["SN"]).count().sum()
+
+def purchaseAnalys():
+    uniqueItems = purData.groupby(["Item Name"]).count().sum()
+    avgPurprice = purData.groupby(["Price"]).avg()
+    totalPur = purData.groupby((["Purchase ID"]).sum()) + 1
+    totalRev = purData.groupby(["Price"]).sum()
+print (purchaseAnalys)
     
