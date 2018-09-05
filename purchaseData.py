f3c6b3c373f0463ec3f126a836bfaecce487b14d
Anirudh Rajagopalan <anirudhrajagopalan@Anirudhs-MacBook-Pro.local>
2018-09-05 04:22:10 -0400

    unique data

--- purchaseData.py
+++ purchaseData.py
@@ -10,25 +10,24 @@ purData = pd.DataFrame(purchaseData)
 
 '''purData.head() # no errors'''
 
+u_I = []
+for x in purData:
+    u_I = purData.append("Item Name").value()
+    unique_I = u_I.index.sum()
+print(unique_I)
 
+'''totalPlayers = purData["SN"].count()
+    t_P = totalPlayers.sum()
+    print (t_P)'''
 
-totalPlayers = purData.groupby(["SN"]).count().sum()
-
-uniqueItems = purData.groupby(["Item Name"]).count().sum()
-avgPurprice = purData.groupby(["Price"]).mean()
-totalPur = purData.len()
-totalRev = purData.groupby(["Price"]).sum()
-
-return (totalPlayers)
-
-
-
-
-def add_numbers(a,b):
-    output = a+b
-    return(output)
-
-
-output = add_numbers(5,4)
-print(output)
+'''avgPur_price = purData["Price"].mean()
+    print (avgPur_price)
     
+    totalPur = len(purData)
+    print (totalPur)
+    
+    totalRev = purData["Price"].sum()
+    print (totalRev)'''
+
+
+
